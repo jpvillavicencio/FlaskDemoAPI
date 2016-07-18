f41b3c9be08663b961cb7932eef5fe20bd602d56
jpvillavicencio <jefferson.villavicencio@gmail.com>
2016-07-18 15:16:52 +1000

    Removed config.py. Changed mongo.py to have Production and Test Database

--- mongo.py
+++ mongo.py
@@ -3,13 +3,15 @@
 # author = JP Villavicencio
 # email = jefferson.villavicencio@team.telstra.com
 # status = Prototype
-# last revision = 15 July 2016
+# last revision = 18 July 2016
 
 import pymongo
 
 # Database location.
-con = pymongo.MongoClient("mongodb://localhost:27017/")
+con_test = "mongodb://localhost:27017/"
+con_prod = "mongodb://192.168.99.100:27017/"
+
+con = pymongo.MongoClient(con_prod)
 
 # Database name.
-mongo_production = con.flask_prod
 mongo_test = con.flask_test
